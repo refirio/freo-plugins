b62ac81f1768e4bcede34a4a2bbc029942668939
minakata <minakata.k-1005@holydragoon.jp>
2019-01-28 16:44:32 +0900

    エントリータグ管理プラグイン configファイル 2019/01/28 16:43 更新分 管理画面からタグを修正・編集したときに、ログインしていないなどの環境からエントリーを閲覧すると、修正前のタグが表示されてしまうため、endファイルの読み込み設定を追加

--- entry_tagmanager/libs/freo/plugins/config.entry_tagmanager.php
+++ entry_tagmanager/libs/freo/plugins/config.entry_tagmanager.php
@@ -2,9 +2,9 @@
 
 /*********************************************************************
 
- エントリータグ管理プラグイン | 設定ファイル (2013/04/14)
+ エントリータグ管理プラグイン | 設定ファイル (2019//)
 
- Copyright(C) 2009-2013 freo.jp
+ Copyright(C) 2009-2019 freo.jp
 
 *********************************************************************/
 
@@ -19,5 +19,10 @@ define('FREO_PLUGIN_ENTRY_TAGMANAGER_ADMIN', 'entry_tagmanager/admin');
 
 //pageファイルの読み込み設定
 define('FREO_PLUGIN_ENTRY_TAGMANAGER_LOAD_PAGE', 'entry_tagmanager');
+
+//holydragoonjp 追加ここから
+//endファイルの読み込み設定
+define('FREO_PLUGIN_ENTRY_TAGMANAGER_LOAD_END', 'entry_tagmanager/admin_post,entry_tagmanager/admin_delete');
+//追加ここまで
 
 ?>
